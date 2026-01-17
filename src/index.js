c99b6c3b457c8d68d3d31cd1fdf0903015c4276b
Alex <alex.stormwood@bigfootds.com>
2026-01-17 11:12:14 +1100

    fix: log of the key

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const { app } = require("./server");
 
 const PORT = process.env.PORT || 3000;
 
+console.log("JWT SECRET KEY is: " + process.env.JWT_SECRET_KEY);
+
 app.listen(PORT, () => {
 	console.log(`Express Dice API is running on http://localhost:${PORT}`);
 });
